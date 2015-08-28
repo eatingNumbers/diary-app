a84f6e681036eb5b52dc1d65106e2bbd6274abf4
Dean Laurea <deanlaurea@icloud.com>
2015-08-28 14:19:21 -0500

    Implement insert feature

---
+++ Diary/Diary/THEntryListViewControllerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  THEntryListViewControllerTableViewController.h
+//  Diary
+//
+//  Created by Dean Laurea on 8/28/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THEntryListViewControllerTableViewController : UITableViewController
+
+@end
