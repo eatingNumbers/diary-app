324a82721aac737c646d6b3262a1b29a4c48760c
Dean Laurea <deanlaurea@icloud.com>
2015-08-31 11:15:17 -0500

    Add navigation bar styles

---
+++ Diary/Diary/THNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  THNavigationController.h
+//  Diary
+//
+//  Created by Dean Laurea on 8/31/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THNavigationController : UINavigationController
+
+@end
---
+++ Diary/Diary/THNavigationController.m
@@ -0,0 +1,21 @@
+//
+//  THNavigationController.m
+//  Diary
+//
+//  Created by Dean Laurea on 8/31/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import "THNavigationController.h"
+
+@interface THNavigationController ()
+
+@end
+
+@implementation THNavigationController
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+@end
